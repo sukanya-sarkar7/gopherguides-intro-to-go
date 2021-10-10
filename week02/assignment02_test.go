35636136cf318f202bbef0c5e7aa4db1dcf4d974
Sarkar, Sukanya <sukanya.sarkar@anz.com>
2021-10-10 14:15:12 +1100

    feat: week02 testing for workflow

--- week02/assignment02_test.go
+++ week02/assignment02_test.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	b := make([]string, 2, 4)
-	b = append(b, "sat", "Mar", "Jup")
+	b = append(b, "sat", "Mar", "Jup", "sam")
 
 	fmt.Println(b)
 	fmt.Println("lens:", len(b))
